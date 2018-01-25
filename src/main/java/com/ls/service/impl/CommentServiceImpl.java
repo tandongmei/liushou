d85dc183ae2fdcbc16af48df9ef974034c995ba0
tandongmei <17775205771@163.com>
2018-01-25 17:43:01 +0800

    up

---
+++ src/main/java/com/ls/service/impl/CommentServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ls.service.impl;
+
+import com.ls.service.ICommentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by tan.dongmei on 2018/1/25
+ */
+@Service
+public class CommentServiceImpl implements ICommentService {
+}
