b4bac2e25db1323492c892f0a57b1ec930eba98d
yuhang <649411629@qq.com>
2018-02-06 20:41:38 +0800

    ip

--- src/main/java/com/ls/util/QiniuUtil.java
+++ src/main/java/com/ls/util/QiniuUtil.java
@@ -8,40 +8,13 @@ import org.springframework.beans.factory.annotation.Value;
 public class QiniuUtil {
 
     @Value("${qiniu.accessKey}")
-    private static String accessKey;
+    private String accessKey;
 
     @Value("${qiniu.secretKey}")
-    private static String secretKey;
+    private String secretKey;
 
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        QiniuUtil.uploadFile("image1.jpg","想和你旅行".getBytes());
-    }
-    /**
-     * 从外部文件中初始化七牛存储相关的配置信息
-     */
-    static{
-       InputStream inputStream =QiniuUtil.class.getClassLoader().getResourceAsStream("application.properties") ;
-       Properties properties=new Properties();
-       try {
-            properties.load(inputStream);
-       } catch (IOException e) {
-            e.printStackTrace();
-       }
-       auth = Auth.create(properties.getProperty(CONFIG_AK), properties.getProperty(CONFIG_SK));
-       Configuration cfg = new Configuration(Zone.zone0());//设置空间上传域名
-       uploadManager = new UploadManager(cfg);
-       bucketName=properties.getProperty(CONFIG_BUCKET);
-       cdn = properties.getProperty(CONFIG_CDN);
-    }
-=======
     @Value("${qiniu.bucket}")
-    private static String bucket;
->>>>>>> c278adeb7b6e38879323d13fb3e91c03861a8174
-
-
-
-
+    private String bucket;
 
 
 }
